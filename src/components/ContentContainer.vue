88c21aecac79460caa0a5d26c6fb485405f41402
Jeongwook Oh <hiphophammer@hotmail.com>
2023-01-22 10:24:57 -0800

    Basic Layout

---
+++ src/components/ContentContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PostCard />
+    <PostCard />
+    <PostCard />
+  </div>
+</template>
+
+<script>
+import PostCard from "./PostCard.vue";
+
+export default {
+  components: {
+    PostCard: PostCard,
+  },
+};
+</script>
+
+<style></style>
