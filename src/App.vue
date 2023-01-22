2356ff3b539753a34d81c6e9de37f4390558e39e
Jeongwook Oh <hiphophammer@hotmail.com>
2023-01-22 07:36:19 -0800

    new project

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,9 @@
-<template>
-  <div class="menu">
-    <a v-for="(a, i) in menus" :key="i">{{ menus[i] }}</a>
-  </div>
-  <DiscountPage />
-  <ModalComp
-    @closeButton="modalNumber = -1"
-    :rooms="rooms"
-    :index="modalNumber"
-  />
-  <div v-for="room in rooms" :key="room.id">
-    <RoomCell @openModal="modalNumber = $event" :room="room" />
-  </div>
-</template>
+<template></template>
 
 <script>
-import list from "./assets/data.js";
-import DiscountPage from "./DiscountPage.vue";
-import ModalComp from "./ModalComp.vue";
-import RoomCell from "./Room.vue";
-
 export default {
   name: "App",
-  data() {
-    return {
-      modalNumber: -1,
-      rooms: list,
-      menus: ["Home", "Products", "About"],
-    };
-  },
-  methods: {
-    increase(i) {
-      this.reports[i]++;
-    },
-  },
-  components: {
-    DiscountPage: DiscountPage,
-    ModalComp: ModalComp,
-    RoomCell: RoomCell,
-  },
+  components: {},
 };
 </script>
 
@@ -51,15 +17,5 @@ export default {
 }
 body {
   margin: 0;
-}
-
-.menu {
-  background: darkslateblue;
-  padding: 15px;
-  border-radius: 5px;
-}
-.menu a {
-  color: white;
-  padding: 10px;
 }
 </style>
